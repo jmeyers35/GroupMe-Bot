0b37c8217026d316014a8c43f48ea704241acb41
Jacob Meyers <jmeyers35@gatech.edu>
2019-05-12 12:14:41 -0400

    tweak model settings

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os
 import requests
 
 app = Flask(__name__)
-MAX_OVERLAP_RATIO = MAX_OVERLAP_RATIO
-MAX_OVERLAP_TOTAL=MAX_OVERLAP_TOTAL
+MAX_OVERLAP_RATIO = 0.4
+MAX_OVERLAP_TOTAL=10
 
 @app.route('/', methods=['POST'])
 def web_hook():
@@ -17,55 +17,55 @@ def web_hook():
             with open('models/anthony_barnum_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Anthony bot says: " + msg)
         elif person == 'chase' or person == 'eric':
             with open('models/chase_m_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Chase bot says: " + msg)
         elif person == 'chris' or person ==  'christopher':
             with open('models/christopher_densmore_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Chris bot says: " + msg)
         elif person == 'jack':
             with open('models/jack_maher_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Jack bot says: " + msg)
         elif person == 'johnny':
             with open('models/johnny_chiles_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Johnny bot says: " + msg)
         elif person == 'lanier':
             with open('models/lanier_freeman_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Lanier bot says: " + msg)
         elif person == 'ryan':
             with open('models/ryan_yeung_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Ryan bot says: " + msg)
         elif person == 'yash':
             with open('models/yash_punjabi_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Yash bot says: " + msg)
         elif person == 'jacob':
             with open('models/jacob_meyers_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
+                msg = text_model.make_sentence(tries=1000, max_overlap_ratio=MAX_OVERLAP_RATIO, max_overlap_total=MAX_OVERLAP_TOTAL)
                 send_message("Jacob bot says: " + msg)
         else:
             send_message("Sorry! I don't recognize that name.")
--- create_models.py
+++ create_models.py
@@ -13,13 +13,13 @@ def createModels(corpiDir, dstDir):
             with open ("{}/{}".format(corpiDir, fname)) as f:
                 text = f.read()
                 text_model = markovify.NewlineText(text)
-                print("##########################################")
-                print("Five generated sentences from " + person_name)
-                for _ in range(5):
-                    print(text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7))
-                # model_json = text_model.to_json()
-                # with open("{}/{}_model.json".format(dstDir, person_name), 'w') as outfile:
-                #     json.dump(model_json, outfile)
+                # print("##########################################")
+                # print("Five generated sentences from " + person_name)
+                # for _ in range(5):
+                #     print(text_model.make_sentence(tries=1000, max_overlap_ratio=0.4, max_overlap_total=10))
+                model_json = text_model.to_json()
+                with open("{}/{}_model.json".format(dstDir, person_name), 'w') as outfile:
+                    json.dump(model_json, outfile)
 
 
 def main():
