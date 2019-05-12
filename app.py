d0372e261dc72667862759c35f89ddaae37b6dd0
Jacob Meyers <jmeyers35@gatech.edu>
2019-05-11 22:48:01 -0400

    tweak max overlap total

--- app.py
+++ app.py
@@ -15,55 +15,55 @@ def web_hook():
             with open('models/anthony_barnum_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'chase' or person == 'eric':
             with open('models/chase_m_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'chris' or person ==  'christopher':
             with open('models/christopher_densmore_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'jack':
             with open('models/jack_maher_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'johnny':
             with open('models/johnny_chiles_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'lanier':
             with open('models/lanier_freeman_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'ryan':
             with open('models/ryan_yeung_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'yash':
             with open('models/yash_punjabi_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         elif person == 'jacob':
             with open('models/jacob_meyers_model.json') as f:
                 model_json = json.load(f)
                 text_model = markovify.NewlineText.from_json(model_json)
-                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=10)
+                msg = text_model.make_short_sentence(140, tries=1000, max_overlap_ratio=0.5, max_overlap_total=7)
                 send_message(msg)
         else:
             send_message("Sorry! I don't recognize that name.")
